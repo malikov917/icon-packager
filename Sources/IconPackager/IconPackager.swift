4e42948b30e2dd195026c8799f6bd6dcb986a57b
Kanstantsin Malikau <kanstanstin.malikau@haiilo.com>
2024-06-23 23:25:22 +0200

    add: v1.2.1

--- Sources/IconPackager/IconPackager.swift
+++ Sources/IconPackager/IconPackager.swift
@@ -2,8 +2,12 @@ import SVGKit
 import UIKit
 
 public struct IconPackager {
-    public static func imageView(name name: String) -> SVGFastImageView? {
-        guard let svgImage = SVGKImage(named: name) else { return nil }
+    public static func imageView(named name: String) -> SVGKFastImageView? {
+        guard let bundleURL = Bundle.module.url(forResource: name, withExtension: "svg") else {
+            print("Error: SVG file named \(name) not found.")
+            return nil
+        }
+        let svgImage = SVGKImage(contentsOf: bundleURL)
         return SVGKFastImageView(svgkImage: svgImage)
     }
 }
